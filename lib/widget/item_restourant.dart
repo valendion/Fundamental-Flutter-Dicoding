8524b1530daaa916fe1f676655801ed544e040d8
valendion <valendion874@gmail.com>
2022-09-20 04:02:33 +0800

    fix reject 1 submission 1

--- lib/widget/item_restourant.dart
+++ lib/widget/item_restourant.dart
@@ -12,72 +12,70 @@ class ItemRestourant extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: InkWell(
-        splashColor: greyPrimary,
-        onTap: () {
-          Navigator.pushNamed(context, detailPage, arguments: restourant);
-        },
-        child: CustomCard(
-          borderRadius: 10,
-          child: Row(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10), // Image border
-                child: SizedBox.fromSize(
-                  size: const Size.fromRadius(48), // Image radius
-                  child: Hero(
-                      tag: restourant.id,
-                      child: Image.network(restourant.pictureId,
-                          fit: BoxFit.cover)),
-                ),
-              ),
-              const SizedBox(
-                width: 8,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    restourant.name,
-                    style: Theme.of(context).textTheme.headline6?.merge(title),
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  ),
-                  Wrap(
-                    crossAxisAlignment: WrapCrossAlignment.center,
-                    children: [
-                      const Icon(Icons.location_pin),
-                      Text(restourant.city,
-                          style: Theme.of(context)
-                              .textTheme
-                              .button
-                              ?.merge(subTitle)),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  ),
-                  Wrap(
-                    crossAxisAlignment: WrapCrossAlignment.center,
-                    children: [
-                      const Icon(Icons.star_rate_rounded),
-                      Text(restourant.rating.toString(),
-                          style: Theme.of(context)
-                              .textTheme
-                              .button
-                              ?.merge(subTitle)),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  ),
-                ],
-              ),
-            ],
+    return CustomCard(
+      onTap: () {
+        Navigator.pushNamed(context, detailPage, arguments: restourant);
+      },
+      borderRadius: 10,
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10), // Image border
+            child: SizedBox.fromSize(
+              size: const Size.fromRadius(48), // Image radius
+              child: Hero(
+                  tag: restourant.id,
+                  child:
+                      Image.network(restourant.pictureId, fit: BoxFit.cover)),
+            ),
           ),
-        ),
+          const SizedBox(
+            width: 8,
+          ),
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  restourant.name,
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context).textTheme.headline5?.merge(title),
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Wrap(
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  children: [
+                    const Icon(Icons.location_pin),
+                    Text(restourant.city,
+                        style: Theme.of(context)
+                            .textTheme
+                            .button
+                            ?.merge(subTitle)),
+                  ],
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Wrap(
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  children: [
+                    const Icon(Icons.star_rate_rounded),
+                    Text(restourant.rating.toString(),
+                        style: Theme.of(context)
+                            .textTheme
+                            .button
+                            ?.merge(subTitle)),
+                  ],
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
